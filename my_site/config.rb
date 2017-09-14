9bba352c4880992c31f1a46cad26092d24c3327b
olofvi <olofvi89@gmail.com>
2017-09-14 16:15:03 +0200

    styled with materialize

--- my_site/config.rb
+++ my_site/config.rb
@@ -4,6 +4,18 @@ activate :autoprefixer do |prefix|
   prefix.browsers = "last 2 versions"
 end
 
+activate :deploy do |deploy|
+  deploy.build_before = true
+  deploy.deploy_method = :git
+end
+
+require 'extensions/build_cleaner'
+
+configure :build do
+  activate :relative_assets
+  activate :build_cleaner
+end
+
 activate :livereload
 
 page '/*.xml', layout: false
